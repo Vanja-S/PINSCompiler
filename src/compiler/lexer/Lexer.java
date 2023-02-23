cddd4133cfb68e7ae7dfe5653b670daecf6b1903
Vanja Stojanović <64472202+Vanja-S@users.noreply.github.com>
2023-02-23 16:23:07 +0100

    Initial commit

---
+++ src/compiler/lexer/Lexer.java
@@ -0,0 +1,63 @@
+/**
+ * @Author: turk
+ * @Description: Leksikalni analizator.
+ */
+
+package compiler.lexer;
+
+import static common.RequireNonNull.requireNonNull;
+import static compiler.lexer.TokenType.*;
+import compiler.lexer.Position.Location;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import common.Report;
+
+public class Lexer {
+    /**
+     * Izvorna koda.
+     */
+    private final String source;
+
+    /**
+     * Preslikava iz ključnih besed v vrste simbolov.
+     */
+    private final static Map<String, TokenType> keywordMapping;
+
+    static {
+        keywordMapping = new HashMap<>();
+        for (var token : TokenType.values()) {
+            var str = token.toString();
+            if (str.startsWith("KW_")) {
+                keywordMapping.put(str.substring("KW_".length()).toLowerCase(), token);
+            }
+            if (str.startsWith("AT_")) {
+                keywordMapping.put(str.substring("AT_".length()).toLowerCase(), token);
+            }
+        }
+    }
+
+    /**
+     * Ustvari nov analizator.
+     * 
+     * @param source Izvorna koda programa.
+     */
+    public Lexer(String source) {
+        requireNonNull(source);
+        this.source = source;
+    }
+
+    /**
+     * Izvedi leksikalno analizo.
+     * 
+     * @return seznam leksikalnih simbolov.
+     */
+    public List<Symbol> scan() {
+        var symbols = new ArrayList<Symbol>();
+        // todo: implementacija leksikalne analize
+        return symbols;
+    }
+}
